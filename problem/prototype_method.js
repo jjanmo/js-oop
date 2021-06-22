0e3455e07d0c16f9012f7031029fec72b86acd9e
jjanmo <jjanmo07@hanmail.net>
2021-06-23 00:54:24 +0900

    feat: Update prototype-method docs

--- problem/prototype_method.js
+++ problem/prototype_method.js
@@ -1,8 +1,19 @@
 // ref : https://ko.javascript.info/prototype-methods
 
-// #1 ??
-let dictionary = Object.create(null);
+// #1 ?? 이 문제는 잘 모르겠음...😰
+// 더 공부 해야할 것!!
+// -> Object.create() 에서의 두번째 인자로 들어가는 것들에 대해서
+// -> 추가1) Object.create() vs new operator : https://medium.com/@jonathanvox01/understanding-the-difference-between-object-create-and-the-new-operator-b2a2f4749358
+// -> 추가2) isPrototypeOf() vs instanceof
+// -> for in과 객체 열거 속성과의 관계
 
+let dictionary = Object.create(null, {
+  toString: {
+    value() {
+      return Object.keys(this).join();
+    },
+  },
+});
 // dictionary.toString 메서드를 추가하는 코드
 
 // 데이터를 추가합니다.
@@ -16,6 +27,8 @@ for (let key in dictionary) {
 
 // toString이 동작하는 부분입니다.
 alert(dictionary); // "apple,__proto__"
+// -> 이 부분에서 dictionary.toString()으로 호출하지 않아도 되는 이유
+// -> alert() 안에서 문자열로 강제형변환되면서 toString()이 자동으로 호출된다!!
 
 // #2
 function Rabbit(name) {
